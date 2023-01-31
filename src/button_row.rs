ad9c795b098347c738b5695b9cb776beeddb7cd6
James Benner <james.benner@gmail.com>
2023-01-31 16:27:57 -0500

    Add reference links in comment

--- src/button_row.rs
+++ src/button_row.rs
@@ -25,6 +25,9 @@ pub fn build_button_row(window: &gtk::ApplicationWindow) -> gtk::Box {
         .halign(gtk::Align::Center)
         .build();
     let directory_button = gtk::Button::builder()
+        // Icon names are here: https://developer-old.gnome.org/gtk3/stable/gtk3-Stock-Items.html
+        // and supposedly here: https://developer.gnome.org/gtk/stable/gtk-Stock-Items.html
+        // if the later ever comes back online.
         .icon_name("folder")
         .margin_top(margin)
         .margin_bottom(margin)
