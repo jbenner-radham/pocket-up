555c8b5ecad3527ef233472288f9174cc72f09ee
James Benner <james.benner@gmail.com>
2023-02-22 20:03:29 -0500

    Add version to about dialog

--- src/about_dialog.rs
+++ src/about_dialog.rs
@@ -1,9 +1,14 @@
 use crate::config::APP_NAME;
 
 pub fn build_about_dialog() -> gtk::AboutDialog {
+    let manifest_src = include_str!("../Cargo.toml");
+    let manifest: toml::Value = toml::from_str(manifest_src).unwrap();
+    let version = manifest["package"]["version"].as_str().unwrap();
+
     gtk::AboutDialog::builder()
         .program_name(APP_NAME)
         .title(format!("About {APP_NAME}"))
+        .version(version)
         .authors(vec!["James Benner https://www.jamesbenner.com/".to_string()])
         .license_type(gtk::License::MitX11)
         .build()
